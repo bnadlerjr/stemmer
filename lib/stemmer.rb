872529e05d2d71822356376d270e61dd953c6e38
Bob Nadler <bob@Bob-Nadlers-MacBook-Pro.local>
2010-07-26 14:54:03 -0400

    Add logic for downcase option.

--- lib/stemmer.rb
+++ lib/stemmer.rb
@@ -3,8 +3,14 @@ require 'lib/stemmer/porter'
 module Stemmer
   VERSION = '1.0.1'
 
+  # Stems a word. Uses the Porter stemming algorithm by default.
+  # algorithm:: The name of the algorithm to use as a symbol. Right now only 
+  #             the Porter algorithm is supported.
+  # downcase:: If true, the string will be downcased before being sent to the
+  #            stemming algorithm. Default value is +false+.
   def stem(algorithm=:porter, downcase=false)
-    Porter.stem(self)
+    downcase ? word = self.downcase : word = self
+    Porter.stem(word)
   end
 end
 
